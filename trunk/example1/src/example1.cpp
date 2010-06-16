95f4b69177c54751c47b71098dba32ebf2f68617
tamzarian1989 <tamzarian1989@51415b2c-dcf7-11dd-b58c-9b3bf6bc8b46>
2010-06-16 04:56:28 +0000

    Update to Example #1

--- trunk/example1/src/example1.cpp
+++ trunk/example1/src/example1.cpp
@@ -12,23 +12,48 @@ int main(int argc, char **argv) {
 	PadSystem *padSystem = new PadSystem();
 
 	FreeTypeGX *fontSystem = new FreeTypeGX(GX_TF_IA8);
+	FT_UInt fontSize = 64;
+	fontSystem->loadFont(rursus_compact_mono_ttf, rursus_compact_mono_ttf_size, fontSize, false);	// Initialize the font system with the font parameters from rursus_compact_mono_ttf.h
 
-	fontSystem->loadFont(rursus_compact_mono_ttf, rursus_compact_mono_ttf_size, 64, false);	// Initialize the font system with the font parameters from rursus_compact_mono_ttf.h
+	uint32_t buttons = 0x0000;
+	bool isUnderlined = false;
+	bool isStrike = false;
 
-	while(!padSystem->pressedExitButton(padSystem->scanPads(0))) {
+	while(!padSystem->pressedExitButton(buttons)) {
 
-		fontSystem->drawText(320,	50,		_TEXT("THE QUICK BROWN"),	(GXColor){0xff, 0x00, 0x00, 0xff},	FTGX_JUSTIFY_CENTER | FTGX_ALIGN_TOP);
-		fontSystem->drawText(320,	125,	_TEXT("FOX JUMPS OVER"),	(GXColor){0x00, 0xff, 0x00, 0xff},	FTGX_JUSTIFY_CENTER | FTGX_ALIGN_MIDDLE);
-		fontSystem->drawText(320,	200,	_TEXT("THE LAZY DOG"),		(GXColor){0x00, 0x00, 0xff, 0xff},	FTGX_JUSTIFY_CENTER | FTGX_ALIGN_BOTTOM);
+		uint32_t textStyle = FTGX_JUSTIFY_CENTER;
+		if(isUnderlined) {
+			textStyle |= FTGX_STYLE_UNDERLINE;
+		}
+		if(isStrike) {
+			textStyle |= FTGX_STYLE_STRIKE;
+		}
 
-		fontSystem->drawText(320,	275,	_TEXT("the quick brown"),	(GXColor){0xff, 0xff, 0x00, 0xff},	FTGX_JUSTIFY_CENTER | FTGX_STYLE_UNDERLINE);
-		fontSystem->drawText(320,	350,	_TEXT("fox jumps over"),	(GXColor){0xff, 0x00, 0xff, 0xff},	FTGX_JUSTIFY_CENTER | FTGX_STYLE_UNDERLINE);
-		fontSystem->drawText(320,	425,	_TEXT("the lazy dog"),		(GXColor){0x00, 0xff, 0xff, 0xff},	FTGX_JUSTIFY_CENTER | FTGX_STYLE_UNDERLINE);
+		fontSystem->drawText(320,	50,		_TEXT("THE QUICK BROWN"),	(GXColor){0xff, 0x00, 0x00, 0xff},	textStyle | FTGX_ALIGN_TOP);
+		fontSystem->drawText(320,	125,	_TEXT("FOX JUMPS OVER"),	(GXColor){0x00, 0xff, 0x00, 0xff},	textStyle | FTGX_ALIGN_MIDDLE);
+		fontSystem->drawText(320,	200,	_TEXT("THE LAZY DOG"),		(GXColor){0x00, 0x00, 0xff, 0xff},	textStyle | FTGX_ALIGN_BOTTOM);
+
+		fontSystem->drawText(320,	275,	_TEXT("the quick brown"),	(GXColor){0xff, 0xff, 0x00, 0xff},	textStyle);
+		fontSystem->drawText(320,	350,	_TEXT("fox jumps over"),	(GXColor){0xff, 0x00, 0xff, 0xff},	textStyle);
+		fontSystem->drawText(320,	425,	_TEXT("the lazy dog"),		(GXColor){0x00, 0xff, 0xff, 0xff},	textStyle);
 
 		graphicsSystem->updateScene(videoSystem->getVideoFramebuffer());
 		videoSystem->flipVideoFramebuffer();
+
+		buttons = padSystem->scanPads(0);
+		if(padSystem->pressedUp(buttons)) {
+			fontSystem->loadFont(rursus_compact_mono_ttf, rursus_compact_mono_ttf_size, ++fontSize, false);
+		}
+		if(padSystem->pressedDown(buttons)) {
+			fontSystem->loadFont(rursus_compact_mono_ttf, rursus_compact_mono_ttf_size, fontSize > 6 ? --fontSize : fontSize, false);
+		}
+		if(padSystem->pressedLeft(buttons)) {
+			isUnderlined = !isUnderlined;
+		}
+		if(padSystem->pressedRight(buttons)) {
+			isStrike = !isStrike;
+		}
 	}
-	while(!padSystem->pressedExitButton(padSystem->scanPads(0))) {}
 
 	delete fontSystem;
 	delete padSystem;
