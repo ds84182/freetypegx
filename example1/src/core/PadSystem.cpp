d7ab81cd4ff240d77463e82b6c86835bd55d9477
tamzarian1989 <tamzarian1989@51415b2c-dcf7-11dd-b58c-9b3bf6bc8b46>
2010-06-16 04:56:28 +0000

    Update to Example #1

--- example1/src/core/PadSystem.cpp
+++ example1/src/core/PadSystem.cpp
@@ -35,3 +35,59 @@ bool PadSystem::pressedExitButton(uint32_t buttonsDown) {
 
 	return false;
 }
+
+bool PadSystem::pressedUp(uint32_t buttonsDown) {
+#ifdef __WIISYSTEM__
+	if (buttonsDown & WPAD_BUTTON_UP) {
+		return true;
+	}
+#else
+	if (buttonsDown & PAD_BUTTON_UP) {
+		return true;
+	}
+#endif
+
+	return false;
+}
+
+bool PadSystem::pressedDown(uint32_t buttonsDown) {
+#ifdef __WIISYSTEM__
+	if (buttonsDown & WPAD_BUTTON_DOWN) {
+		return true;
+	}
+#else
+	if (buttonsDown & PAD_BUTTON_DOWN) {
+		return true;
+	}
+#endif
+
+	return false;
+}
+
+bool PadSystem::pressedLeft(uint32_t buttonsDown) {
+#ifdef __WIISYSTEM__
+	if (buttonsDown & WPAD_BUTTON_LEFT) {
+		return true;
+	}
+#else
+	if (buttonsDown & PAD_BUTTON_LEFT) {
+		return true;
+	}
+#endif
+
+	return false;
+}
+
+bool PadSystem::pressedRight(uint32_t buttonsDown) {
+#ifdef __WIISYSTEM__
+	if (buttonsDown & WPAD_BUTTON_RIGHT) {
+		return true;
+	}
+#else
+	if (buttonsDown & PAD_BUTTON_RIGHT) {
+		return true;
+	}
+#endif
+
+	return false;
+}
--- example1/src/core/PadSystem.h
+++ example1/src/core/PadSystem.h
@@ -16,5 +16,9 @@ class PadSystem {
 		PadSystem();
 		uint32_t scanPads(int controller);
 		bool pressedExitButton(uint32_t buttons);
+		bool pressedUp(uint32_t buttons);
+		bool pressedDown(uint32_t buttons);
+		bool pressedLeft(uint32_t buttons);
+		bool pressedRight(uint32_t buttons);
 };
 #endif /*PADSYSTEM_H_*/
